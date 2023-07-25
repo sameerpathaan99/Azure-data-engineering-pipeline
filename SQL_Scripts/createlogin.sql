e54006b836fda656c631f2463b65f9cc6b6492a8
sameer khan <sameerde@zohomail.com>
2023-07-25 02:54:05 -0500

    pushing code

---
+++ SQL_Scripts/createlogin.sql
@@ -0,0 +1,3 @@
+CREATE USER 'sameer'@'localhost' IDENTIFIED BY 'cbfhsbcdhcbercbnj';
+CREATE USER 'sameer'@'%' IDENTIFIED BY 'cbfhsbcdhcbercbnj';
+GRANT ALL PRIVILEGES ON database_name.* TO 'sameer'@'localhost';
